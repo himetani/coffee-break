1aa1713b41b86d590df7d8532afbd5ed5b80c719
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-06-27 08:54:53 +0900

    tweak static serving

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 //go:generate go-assets-builder -s="/static" -o assets.go static/
 
-var isProd bool
+var (
+	isProd bool
+	db     *gorm.DB
+)
 
 func init() {
 	flag.BoolVar(&isProd, "prod", false, "print version")
@@ -24,29 +29,31 @@ func main() {
 
 	if isProd {
 		fmt.Println("serve from Assets")
-		e.GET("/", echo.WrapHandler(http.FileServer(Assets)))
+		e.GET("/*", echo.WrapHandler(http.FileServer(Assets)))
 	} else {
 		fmt.Println("serve from static dir")
-		e.GET("/", echo.WrapHandler(http.FileServer(http.Dir("./static"))))
+		e.Static("/", "static")
 	}
 
 	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
 	e.GET("/api/reservations", get)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func debug() []reservation {
-	reservations := []reservation{
-		reservation{"06/26/2018", "himetani"},
-		reservation{"06/27/2018", "taro"},
+func debug() []Reservation {
+	reservations := []Reservation{
+		Reservation{Date: "06/26/2018", Name: "himetani"},
+		Reservation{Date: "06/27/2018", Name: "taro"},
 	}
 
 	return reservations
 
 }
 
-type reservation struct {
+type Reservation struct {
 	Date string `json:"date"`
 	Name string `json:"name"`
 }
@@ -56,5 +63,6 @@ func get(c echo.Context) error {
 }
 
 func create(c echo.Context) error {
+
 	return nil
 }
